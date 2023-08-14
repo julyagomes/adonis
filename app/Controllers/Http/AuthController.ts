d13c4bfccd8fbc60f97d40adeacb28a1ddc931d2
julyagomes <julyamodena01@gmail.com>
2023-08-14 09:41:20 -0300

    adonis

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,26 +1,25 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import User from 'App/Models/User'
+import User from '../../Models/User'
 import UserValidator from '../../Validators/UserValidator'
 
 export default class AuthController {
-  public async register({request}: HttpContextContract){
-    const data = await request.validate(UserValidator)
-    const user = await User.create(data)
-    return user
-
+  public async register({request}: HttpContextContract) {
+     const data = await request.validate(UserValidator)
+     const user = await User.create(data)
+     return user
   }
-
-  public async login({}: HttpContextContract){
-    try {
+  
+  public async login({request, response, auth}: HttpContextContract){
+    // try{
       const {email, password} = request.all()
-      const token = await authConfig.use('api').attempt(email, password, {
+      const token = await auth.use('api').attempt(email, password, {
         expiresIn: '1day'
       })
-      const user = await User.findByOrFail('email', email)
-      return {token, user}
-    } catch (error) {
-      response.status(401).send("Login ou senha incorretos!!!")
-    }
-    }
+      const  user = await User.findByOrFail('email',email)
+      return {token, user}  
+    // } catch (error){
+        // response.status(401).send("Login ou senha incorreta!!!")
+    // }
   }
-
+ 
+}
